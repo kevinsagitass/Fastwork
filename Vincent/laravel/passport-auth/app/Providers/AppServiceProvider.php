e9016c157269c03718f97ed2414210b367fe37b6
U072753 <kevinsagita79@gmail.com>
2023-01-04 16:24:27 +0700

    Update Passport Authenticate API

---
+++ Vincent/laravel/passport-auth/app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->register();
+
+        Passport::tokensExpireIn(now()->addYears(10));
+
+        Passport::refreshTokensExpireIn(now()->addYears(10));
+    }
+}
---
+++ Vincent/laravel/passport-auth/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\UserAuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/register', [UserAuthController::class, 'register'])->name('register');
+Route::post('/login', [UserAuthController::class, 'login'])->name('login');
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('/test', function () {
+        return 'Hello World';
+    });
+});
+
